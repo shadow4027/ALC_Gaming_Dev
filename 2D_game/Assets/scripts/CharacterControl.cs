40e8b532df54963e3505451b60b66a2ee682f4e0
jeremy <jbloom520@gmail.com>
2018-09-11 09:23:20 -0600

    Unity asset change

--- 2D_game/Assets/scripts/CharacterControl.cs
+++ 2D_game/Assets/scripts/CharacterControl.cs
@@ -5,12 +5,34 @@ using UnityEngine;
 public class CharacterControl : MonoBehaviour {
 
 	// Use this for initialization
+
+	//player movement variables
+	public float moveSpeed;
+	public float JumpHeight = 6; 
+
+//player ground variables 
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask whatIsGround;
+	private bool grounded;
+
+	// //non-stick player 
+	// private float moveVelocity;
+	
 	void Start () {
 		
 	}
 	
+	void FixedUpdate() {
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+	}
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetKeyDown (KeyCode.Space) && grounded) {
+			Jump();
+		}
+	}
+	public void Jump () {
+		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight);
 	}
 }
