725f85522855d50f3eda7428460bcfeb1c5d46a8
jeremy <jbloom520@gmail.com>
2018-09-19 09:24:50 -0600

    updated script Character Control

--- 2D_game/Assets/scripts/CharacterControl.cs
+++ 2D_game/Assets/scripts/CharacterControl.cs
@@ -7,7 +7,7 @@ public class CharacterControl : MonoBehaviour {
 	// Use this for initialization
 
 	//player movement variables
-	public float moveSpeed;
+	public float moveSpeed = 3;
 	public float JumpHeight = 6; 
 
 //player ground variables 
@@ -31,8 +31,14 @@ public class CharacterControl : MonoBehaviour {
 		if(Input.GetKeyDown (KeyCode.Space) && grounded) {
 			Jump();
 		}
+		if(Input.GetKeyDown (KeyCode.D)) {
+			MoveRight();
+		}
 	}
 	public void Jump () {
 		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight);
+	}
+	public void MoveRight () {
+		GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
 	}
 }
